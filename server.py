cc005a000c47206ee782dba1a0a834215308e54d
Jacques Wagener <jacques@dilectum.co.za>
2021-11-03 10:56:28 +0200

    Linting fixes

--- server.py
+++ server.py
@@ -24,33 +24,34 @@ async def handle(request):
 
 
 def _compile(data):
-        code = data.get('code')
-        if not code:
-            return {'status': 'failed', 'message': 'No "code" key supplied'}, 400
-        if not isinstance(code, str):
-            return {'status': 'failed', 'message': '"code" must be a non-empty string'}, 400
+    code = data.get('code')
+    if not code:
+        return {'status': 'failed', 'message': 'No "code" key supplied'}, 400
+    if not isinstance(code, str):
+        return {'status': 'failed', 'message': '"code" must be a non-empty string'}, 400
 
-        try:
-            out_dict = compile_code(code, ['abi', 'bytecode', 'bytecode_runtime', 'ir', 'method_identifiers'])
-            out_dict['ir'] = str(out_dict['ir'])
-        except VyperException as e:
-            if e.col_offset and e.lineno:
-                col_offset, lineno = e.col_offset, e.lineno
-            elif e.annotations and len(e.annotations) > 0:
-                ann = e.annotations[0]
-                col_offset, lineno = ann.col_offset, ann.lineno
-            else:
-                col_offset, lineno = None, None
-            return {
-                'status': 'failed',
-                'message': str(e),
-                'column': col_offset,
-                'line': lineno
-            }, 400
+    try:
+        out_dict = compile_code(code, ['abi', 'bytecode', 'bytecode_runtime', 'ir', 'method_identifiers'])
+        out_dict['ir'] = str(out_dict['ir'])
+    except VyperException as e:
+        if e.col_offset and e.lineno:
+            col_offset, lineno = e.col_offset, e.lineno
+        elif e.annotations and len(e.annotations) > 0:
+            ann = e.annotations[0]
+            col_offset, lineno = ann.col_offset, ann.lineno
+        else:
+            col_offset, lineno = None, None
+        return {
+            'status': 'failed',
+            'message': str(e),
+            'column': col_offset,
+            'line': lineno
+        }, 400
 
-        out_dict.update({'status': "success"})
+    out_dict.update({'status': "success"})
 
-        return out_dict, 200
+    return out_dict, 200
+
 
 @routes.route('OPTIONS', '/compile')
 async def compile_it_options(request):
